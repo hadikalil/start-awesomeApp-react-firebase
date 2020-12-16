8c38556c135c7fcf3dfb38ad95d17ba2cf6de5e1
hadi kalil <hadikalilweb@gmail.com>
2020-12-16 21:17:59 +0200

    tracking user sessions with useAuth Hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,14 +1,13 @@
 import React from "react";
 import useFormValidation from "./useFormValidation";
 import validateLogin from "./validateLogin";
-import firebase from '../../firebase'
+import firebase from "../../firebase";
 
 const INITIAL_STATE = {
   name: "",
   email: "",
   password: "",
 };
-
 
 function Login(props) {
   const {
@@ -20,40 +19,49 @@ function Login(props) {
     isSubmitting,
   } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser);
   const [login, setLogin] = React.useState(true);
-// to display the error masseges we need a  state 
-  const [firebaseError, setFirebaseError] = React.useState(null) 
+  // to display the error masseges we need a  state
+  const [firebaseError, setFirebaseError] = React.useState(null);
 
-  // authenticateUser IS going to call either the logging method or the register method of out firebase instance 
-      // to determine which to call we're going to take the value of log in  and we'll use a ternayoperator 
+  // authenticateUser IS going to call either the logging method or the register method of out firebase instance
+  // to determine which to call we're going to take the value of log in  and we'll use a ternayoperator
 
-
-
-
-
-  async function authenticateUser() { 
-    
-    const { name, email, password } = values
+  async function authenticateUser() {
+    const { name, email, password } = values;
     try {
       login
-      ? await firebase.login(email, password)
-      : await firebase.register(name, email, password);
+        ? await firebase.login(email, password)
+        : await firebase.register(name, email, password);
+      props.history.push("/"); // this takes us to the desired route
     } catch (err) {
-      console.error('Authentication Error ',err);
-      setFirebaseError(err.message)     
+      console.error("Authentication Error ", err);
+      setFirebaseError(err.message);
     }
   }
-// before the use of try and catch/ before error handling :*
-//**********************************
-// async function authenticateUser() { 
-    //   const { name, email, password } = values
-//   const response = login 
-//       ? await firebase.login(email, password)
-//       : await firebase.register(name, email, password);
-//     console.log({response});
-// }
-//*************************************
 
+  // before adding tracking user sessions with useAuth Hook
+  // async function authenticateUser() {
 
+  //   const { name, email, password } = values
+  //   try {
+  //     login
+  //     ? await firebase.login(email, password)
+  //     : await firebase.register(name, email, password);
+  //   } catch (err) {
+  //     console.error('Authentication Error ',err);
+  //     setFirebaseError(err.message)
+  //   }
+  // }
+
+  // before the use of try and catch/ before error handling :*
+  //**********************************
+  // async function authenticateUser() {
+  //   const { name, email, password } = values
+  //   const response = login
+  //       ? await firebase.login(email, password)
+  //       : await firebase.register(name, email, password);
+  //     console.log({response});
+  // }
+  //*************************************
 
   return (
     <div>
@@ -68,7 +76,7 @@ function Login(props) {
             placeholder="Your name"
             autoComplete="off"
           />
-        )}        
+        )}
         <input
           onChange={handleChange}
           onBlur={handleBlur}
@@ -92,7 +100,12 @@ function Login(props) {
         {errors.password && <p className="error-text">{errors.password}</p>}
         {firebaseError && <p className="error-text">{firebaseError}</p>}
         <div className="flex mt3">
-          <button type="submit" className="button pointer mr2" disabled={isSubmitting} style={{background: isSubmitting ? "grey" : "orange"}} >
+          <button
+            type="submit"
+            className="button pointer mr2"
+            disabled={isSubmitting}
+            style={{ background: isSubmitting ? "grey" : "orange" }}
+          >
             Submit
           </button>
           <button
--- src/components/Auth/useAuth.js
+++ src/components/Auth/useAuth.js
@@ -1,5 +1,32 @@
 import React from "react";
+// import the firebase instance :-
+import firebase from "../../firebase";
 
-function useAuth() {}
+// onAuthStateChanged listner ==> it's going to detect whethher a user is logged in or created an account 
+// create a hook for the sake of listening as to whether we have an authenticated user or not 
+
+function useAuth() {
+    const [authUser, setAuthUser] = React.useState(null);
+    // within use effect there is listener . we want to remove it whenever we've unbounded whenever we don't need to use this hook 
+    // so we made the unsubscribe varibal and to get rid of it at he bottom 
+    // we return a function to clean up if necessarly 
+    React.useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                setAuthUser(user);
+            } else {
+                setAuthUser(null);
+            }
+        });
+
+        return () => unsubscribe(); // so now we're not executing our listener unnecessarily
+    }, []);
+
+    return authUser;
+    // thake that user and provide it to the rest of our app
+}
 
 export default useAuth;
+
+
+// to see how it works go to LinkList.js
