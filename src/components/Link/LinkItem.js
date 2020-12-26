d6fdee41cdb04d2761a98a16e688d393917ac43d
hadi kalil <hadikalilweb@gmail.com>
2020-12-26 23:16:53 +0200

    first fetching links with .onSnapshot() ofter   displayng links -formatting urls and dates --->

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { getDomain } from "../../utils"; // getDomain is a function to make the url desplay pretter  in the link component
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now"; // formatting the timestamp  from library called date functions
+import FirebaseContext from "../../firebase/context";
 
-function LinkItem() {
-  return <div>LinkItem</div>;
+function LinkItem({ link, index, showCount, history }) {
+  // in order to update firestore database we need a refercnce to the firebase instance--> we'll bring that in by using context
+  // well destructur both firebase and the current user data
+  const { firebase, user } = React.useContext(FirebaseContext);
+
+  function handleVote() {
+    //which users will cloik on in order to upvote a given link
+    if (!user) {
+      history.push("/login"); // to history we need to import  withRouter from react-router-dom and to warp the export default withRouter(LinkItem) after we need to d structure from out props the history object
+    } else {
+      console.log("linkitem ", user);
+      ///////////////////////////////UPDATE PATTERN ///////////////////////////////////////////
+      const voteRef = firebase.db().collection("links").doc(link.id); // creating a reference
+      voteRef.get().then((doc) => {
+        // doing get on the voteRef --> get()  return a promise so we could make  this an async function but this time will just add then --> and the callback to then we get access to our returnd document
+        if (doc.exists) {
+          // exists is a property that's made available by firestore on documents --> it fives the value true if it was able to be found successfully
+          const previousVotes = doc.data().votes;
+          const vote = { votedBy: { id: user.uid, name: user.displayName } };
+          const updatedVotes = [...previousVotes, vote];
+          voteRef.update({ votes: updatedVotes });
+        }
+      });
+    }
+  }
+
+  return (
+    <div className="flex items-start mt2">
+      <div className="flex items-center">
+        {showCount && <span className="gray">{index}</span>}
+        <div className="vote-button" onClick={handleVote}>
+          ▲
+        </div>
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description}{" "}
+          <span className="link">({getDomain(link.url)})</span>
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes by {link.postedBy.name}{" "}
+          {distanceInWordsToNow(link.created)}
+          {" | "}
+          <Link to={`/link/${link.id}`}>
+            {link.comments.length > 0
+              ? `${link.comments.length} comments`
+              : "discuss"}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,10 +1,15 @@
 // after we did the creatLink and we have the data at the fire base we need to get it to the linklist
 import React from "react";
 import { FirebaseContext } from "../../firebase";
+import LinkItem from "./LinkItem";
 
 function LinkList(props) {
   // first we need the firebase instins so we need to consume from FirebaseContext ----// we need the firebase instance in order to make a reqest to the database
   const { firebase } = React.useContext(FirebaseContext);
+
+  // second after fitching the data with onSnapshot() ---> we need to display it --> so -->>
+  // so we need a state for our links to live in
+  const [links, setLinks] = React.useState([]); // this will be an empty array --> set by  handleSnapshot()
 
   // making a request when linkList component mounts
   React.useEffect(() => {
@@ -13,19 +18,34 @@ function LinkList(props) {
 
   function getLinks() {
     //firebase.db().collection('links').get() *********get returns a promise  and we can await the promise
-    firebase.db().collection("links").onSnapshot(handleSnapshot); //*********** onSnapshot() method to get the data ==> it sets up an active listener - like the onAuthStateChanged() listener that we used at useAuth.js
-    // onSnapshot() method it's going to listen for any changes with the collection // so when we add new link it's going to be immediately fetched and desplayd in LinkList
+    firebase.db().collection("links").onSnapshot(handleSnapshot); //*********** .onSnapshot() method to get the data ==> it sets up an active listener - like the onAuthStateChanged() listener that we used at useAuth.js
+    // .onSnapshot() method it's going to listen for any changes with the collection // so when we add new link it's going to be immediately fetched and desplayd in LinkList
   }
 
   function handleSnapshot(snapshot) {
-    // onSnapshot() we get a callback function where we can work with all of the documents on a given collection **> snapshot.docs is an array
+    // .onSnapshot() we get a callback function where we can work with all of the documents on a given collection **> snapshot.docs is an array
     const links = snapshot.docs.map((doc) => {
       return { id: doc.id, ...doc.data() }; // id:doc.id, will add to the object the collection-link-id
     });
-    console.log({ links });
+    setLinks(links);
+    
+
   }
 
-  return <div>linklist</div>;
+  return (
+    <div>
+    { links.map((link, index) => (
+      <LinkItem
+        key={link.id}
+        showCount={true}
+        link={link}
+        index={index + 1}
+      /> 
+    ))}
+    </div>
+  );
 }
 
 export default LinkList;
+
+
