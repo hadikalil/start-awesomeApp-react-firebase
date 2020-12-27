0d453e25fb44de0fe255d665eaf4a4b1f6a287ee
hadi kalil <hadikalilweb@gmail.com>
2020-12-27 13:32:05 +0200

    ordering, sorting links - new and top

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -10,6 +10,8 @@ function LinkList(props) {
   // second after fitching the data with onSnapshot() ---> we need to display it --> so -->>
   // so we need a state for our links to live in
   const [links, setLinks] = React.useState([]); // this will be an empty array --> set by  handleSnapshot()
+  const isNewPage = props.location.pathname.includes("new")// true/false i think
+
 
   // making a request when linkList component mounts
   React.useEffect(() => {
@@ -18,8 +20,13 @@ function LinkList(props) {
 
   function getLinks() {
     //firebase.db().collection('links').get() *********get returns a promise  and we can await the promise
-    firebase.db().collection("links").onSnapshot(handleSnapshot); //*********** .onSnapshot() method to get the data ==> it sets up an active listener - like the onAuthStateChanged() listener that we used at useAuth.js
+    //firebase.db().collection("links").onSnapshot(handleSnapshot); //*********** .onSnapshot() method to get the data ==> it sets up an active listener - like the onAuthStateChanged() listener that we used at useAuth.js
     // .onSnapshot() method it's going to listen for any changes with the collection // so when we add new link it's going to be immediately fetched and desplayd in LinkList
+    firebase
+      .db()
+      .collection("links")
+      .orderBy("created", "desc")
+      .onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(snapshot) {
@@ -28,20 +35,27 @@ function LinkList(props) {
       return { id: doc.id, ...doc.data() }; // id:doc.id, will add to the object the collection-link-id
     });
     setLinks(links);
-    
-
   }
+
+  function renderLinks() {// the order go by if the current page isNewPage do the map on the orderd by date if not we will order it by votes in the follwoing 
+      if (isNewPage) {
+          return links
+      }
+      const topLinks = links.slice().sort((l1 , l2) => l2.votes.length - l1.votes.length)// the reason to call the  slice method because sort mutates the original array so we need to copy links with slice first
+      return topLinks
+    }
+
 
   return (
     <div>
-    { links.map((link, index) => (
-      <LinkItem
-        key={link.id}
-        showCount={true}
-        link={link}
-        index={index + 1}
-      /> 
-    ))}
+      {renderLinks().map((link, index) => (
+        <LinkItem
+          key={link.id}
+          showCount={true}
+          link={link}
+          index={index + 1}
+        />
+      ))}
     </div>
   );
 }
